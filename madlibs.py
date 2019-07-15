9e4e23afeac1cff2034edf8d170842963e812404
AnonArtist <52931695+AnonArtist@users.noreply.github.com>
2019-07-15 15:35:18 -0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color. ")
+pluralNoun = input("Enter a noun. ")
+name = input("Enter a name. ")
+
+print("My favourite color is", color)
+print("I am a(n)", pluralNoun)
+print("My name is", name)
