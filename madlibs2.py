dd43315d3e5169e09f9f2310d3e5930f16682e8b
AnonArtist <52931695+AnonArtist@users.noreply.github.com>
2019-07-15 15:44:48 -0600

    Add files via upload

---
+++ madlibs2.py
@@ -0,0 +1,8 @@
+noun = input("Enter a noun. ")
+adjective = input("Enter an adjective. ")
+noun2 = input("Enter another noun. ")
+
+print("After hiding the painting in his", noun)
+print("for two years, he grew", adjective)
+print("and tried to sell it to a/an", noun2)
+print("in Florence, but was caught.")
